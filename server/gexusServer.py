6eca2404dbaa32bb387a89913cc0abee9ad0af4c
Thomas Hauth <Thomas.Hauth@web.de>
2013-05-17 14:47:08 +0200

    exporting the css file as static resources

--- server/gexusServer.py
+++ server/gexusServer.py
@@ -1,27 +1,37 @@
 import cherrypy
+import os
 
 from mako.template import Template
 from mako.lookup import TemplateLookup
 
 
-class CherryExposedBase ( object ):
+class GameDesc (object):
+    def __init__(self, theId, theName, theDescription):
+        self.name = theName
+        self.id = theId
+        self.description = theDescription
+        
+
+
+class CherryExposedBase (object):
     def __init__(self, makoLookup):
         self.m_makoLookup = makoLookup
         
     def getMakoLookup(self):
         return self.m_makoLookup
     
-    def getTemplate (self , name ):
+    def getTemplate (self , name):
         return self.getMakoLookup().get_template(name)
 
-class GameOne (CherryExposedBase):
+class Pong (CherryExposedBase):
     
     def __init__(self, makoLookup):
-        super (GameOne, self).__init__(makoLookup)
+        super (Pong, self).__init__(makoLookup)
     
     @cherrypy.expose
     def index(self):
-        return "Game One"
+        mytemplate = self.getTemplate("pong/pong_controls.html")
+        return mytemplate.render()
 
     # the call http://localhost:8080/GameOne/input/23/23 is mapped to this one !
     @cherrypy.expose
@@ -31,15 +41,19 @@ class GameOne (CherryExposedBase):
 
 
 class Gexus(CherryExposedBase):
+
+    # def __init__(self):
+    #    pass
     
     def __init__(self, makoLookup):
-        super (Gexus, self).__init__(makoLookup)
+        super (Gexus, self).__init__(makoLookup)    
+        self.m_registeredGames = [GameDesc("Pong", "Pong", "come on, it's a classic !")]
     
     @cherrypy.expose
     def index(self):
         
-        mytemplate = self.getTemplate( "gexus.html" )
-        return mytemplate.render()
+        mytemplate = self.getTemplate("gexus.html")
+        return mytemplate.render(gamesAvail=self.m_registeredGames)
         
 
 
@@ -47,13 +61,36 @@ class Gexus(CherryExposedBase):
 def buildStructure(makoLookup):
     
     root = Gexus(makoLookup)
-    root.GameOne = GameOne(makoLookup)
+    root.m_makoLookup = makoLookup
+    root.Pong = Pong(makoLookup)
     
     return root
+
+
+
+#                         tools.staticfile.on: True
+# tools.staticfile.filename: '/home/poseidon/dev/gexus/server/css/base.css'
+#                       })
 
 
 
 mylookup = TemplateLookup(directories=['html/'])  # use for py byetcode compile, module_directory='/tmp/mako_modules')
 root = buildStructure(mylookup)
 
-cherrypy.quickstart(root)
+cherrypy.config.update({'server.socket_host':'192.168.1.33',
+                        'server.socket_port': 8080}  # # be root ot have 80 on *nix
+                        )
+
+appPath = os.path.abspath(os.path.dirname(__file__))
+cssPath = appPath + "/css/"
+
+
+# cherrypy.tree.mount(DoNothing(), '/',
+conf = { '/css/base.css' : {
+                 'tools.staticfile.on': True,
+                 'tools.staticfile.filename' : cssPath + 'base.css'
+                 # 'tools.staticdir.index': 'index.html',
+             }}
+
+
+cherrypy.quickstart(root, config=conf)
